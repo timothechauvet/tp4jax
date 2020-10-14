447ca8c586bec2638bf474eb98c78af31dc61a67
timothecht <timothe.chauvet@efrei.net>
2020-10-14 08:46:42 +0200

    init

---
+++ src/main/java/com/efrei/tp4jax/Tp4jaxApplication.java
@@ -0,0 +1,13 @@
+package com.efrei.tp4jax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4jaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4jaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efrei/tp4jax/Tp4jaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efrei.tp4jax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4jaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
