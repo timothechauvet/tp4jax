8dba2a735a3c9e9f8a565e5737274f0c02559d82
camille_mathieu_efrei_net <camille.mathieu@efrei.net>
2020-10-21 10:47:35 +0200

    tentative with 5

--- src/main/java/com/efrei/tp4jax/InterfaceWebService.java
+++ src/main/java/com/efrei/tp4jax/InterfaceWebService.java
@@ -2,15 +2,49 @@ package com.efrei.tp4jax;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.bind.annotation.XmlSeeAlso;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@WebService
+import com.oracle.xmlns.webservices.jaxws_databinding.ObjectFactory;
+
+@WebService(
+targetNamespace = "http://localhost:8080/WS/MonServiceWeb/")
+@SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
+@XmlSeeAlso({
+  ObjectFactory.class
+})
 public interface InterfaceWebService {
-	@WebMethod
-	public void createEntity(@WebParam(name = "nameImport") String nameImport);
+	@WebMethod(
+	action = "http://localhost:8080/WS/MonServiceWeb/" + "displayBirds")
+	@WebResult(
+	name = "displayBirdsResponse",
+	targetNamespace = "http://localhost:8080/WS/MonServiceWeb/",
+	partName = "parameters")
+	public void displayBirds();
+	//public void createEntity(@WebParam(name = "nameImport") String nameImport);
 
 	
 }
+/*
+@WebService(
+name = "EmployeeServiceTopDown", 
+targetNamespace = "http://topdown.server.jaxws.baeldung.com/")
+@SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
+@XmlSeeAlso({
+  ObjectFactory.class
+})
+public interface InterfaceWebService {
+  @WebMethod(
+    action = "http://topdown.server.jaxws.baeldung.com/"
+    + "EmployeeServiceTopDown/countEmployees")
+  @WebResult(
+    name = "countEmployeesResponse", 
+    targetNamespace = "http://topdown.server.jaxws.baeldung.com/", 
+    partName = "parameters")
+  public int countEmployees();
+}*/
--- src/main/java/com/efrei/tp4jax/MyServiceWebImpl.java
+++ src/main/java/com/efrei/tp4jax/MyServiceWebImpl.java
@@ -9,11 +9,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-@WebService(endpointInterface = "com.efrei.tp4jax.MyServiceWeb")
+@WebService(endpointInterface = "com.efrei.tp4jax.MyServiceWeb",
+targetNamespace = "http://localhost:8080/WS/MonServiceWeb/")
 public class MyServiceWebImpl implements InterfaceWebService{
 	@WebMethod
+	public void displayBirds() {
+		//code
+	}
 	public @WebResult(name="bird") void createEntity(@WebParam(name = "nameImport") String nameImport) {
 		Bird birdy = new Bird(nameImport);
 		System.out.println("Un grand vide dans notre cœur : " + birdy.toString());
 	}
 }
+/*@WebService(
+name = "EmployeeServiceTopDown", 
+endpointInterface = "com.baeldung.jaxws.server.topdown.EmployeeServiceTopDown",
+targetNamespace = "http://topdown.server.jaxws.baeldung.com/")
+public class EmployeeServiceTopDownImpl 
+implements EmployeeServiceTopDown {
+
+  @Inject 
+  private EmployeeRepository employeeRepositoryImpl;
+
+  @WebMethod
+  public int countEmployees() {
+      return employeeRepositoryImpl.count();
+  }
+}*/
